263dd5a857327e070e57bb703d4381b681f5e51b
Avi Vashishta <avivashishta@gmail.com>
2024-02-15 21:38:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+document.getElementById("enterBtn")
+	.addEventListener("click",()=>{
+		document.getElementById("status").innerHTML = "<h1>Entered Metaverse</h1>"
+	})
